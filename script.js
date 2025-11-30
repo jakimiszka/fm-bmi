5782c9880c7dacdb33be2341770380e8de8750c1
jakimiszka <jarek.jakimicha@gmail.com>
2025-11-30 11:27:58 +0100

    finishing layout

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const clacForm = document.querySelector('.calc--form');
-const weightImperial = document.querySelector('.calc--form__imperial--weight');
-const heightImperial = document.querySelector('.calc--form__imperial--height');
-const weightMetric = document.querySelector('.calc--form__metric--weight');
-const heightMetric = document.querySelector('.calc--form__metric--height');
+const imperial = document.querySelector('.calc--form__imperial');
+const metric = document.querySelector('.calc--form__metric');
 
 document.querySelectorAll('input[type="radio"]').forEach(radio => {
   radio.addEventListener('change', function() {
     if (radio.id === 'metric') {
-      // weightImperial.style.display = 'none';
-      // heightImperial.style.display = 'none';
-      // weightMetric.style.display = 'flex';
-      // heightMetric.style.display = 'flex';
+      imperial.style.display = 'none';
+      metric.style.display = 'flex';
     } else if (radio.id === 'imperial') {
-      // weightImperial.style.display = 'flex';
-      // heightImperial.style.display = 'flex';
-      // weightMetric.style.display = 'none';
-      // heightMetric.style.display = 'none';
+       imperial.style.display = 'flex';
+       metric.style.display = 'none';
     }
   });
 });
